9a7d6db278609036ebcf1fdc052204afdec2a1e7
Diego Alberto Cruz <dacruzt@outlook.com>
2020-10-29 14:08:45 -0500

    Update CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
-import playIcon from '../assets/static/play-icon.png';
-import plusIcon from '../assets/static/plus-icon.png'
 
-const CarouselItem = ({ name,picture, username, locationName,professionalHeadline,remoter}) => (
+
+const CarouselItem = ({ name,picture, username, locationName,professionalHeadline}) => (
   <div className="carousel-item">
     <img className="carousel-item__img" src={picture} alt={username} />
     <div className="carousel-item__details">
       <div>
-      <p className="carousel-item__details--title">{username}</p>
-      <p className="carousel-item__details--subtitle">{name}</p>
+        <p className="carousel-item__details--title">{username}</p>
+        <p className="carousel-item__details--subtitle">{name}</p>
       </div>
       <p className="carousel-item__details--title">{professionalHeadline}</p>
       <p className="carousel-item__details--subtitle">{`${locationName} `}</p>
@@ -22,7 +21,6 @@ CarouselItem.propTypes = {
   name: PropTypes.string,
   username: PropTypes.string,
   professionalHeadline: PropTypes.string,
-  remoter: PropTypes.string,
   locationName: PropTypes.string,
   picture: PropTypes.string,
 };
